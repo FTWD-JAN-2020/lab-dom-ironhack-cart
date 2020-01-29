b8b447bada5e9f3820dfc4ae820f64d5c50ad2ce
Tzikas <niko.tzikas@gmail.com>
2020-01-29 18:02:30 -0500

    dom

--- starter-code/js/index.js
+++ starter-code/js/index.js
@@ -22,12 +22,55 @@ function updateSubtot(product) {
 
 function calcAll() {
   // Iteration 1.2
-  let product = document.querySelector("#cart > tbody > tr");
-  let sub = document.querySelector("#cart > tbody > tr > td.subtot > span"); 
-  let justText = document.querySelector("#cart > tbody > tr > td.subtot > span").innerHTML
-  console.log(typeof sub, typeof justText)
-  sub.innerText = updateSubtot(product);
+  // let product = document.querySelector("#cart > tbody > tr");
+  // let sub = document.querySelector("#cart > tbody > tr > td.subtot > span"); 
+  // let justText = document.querySelector("#cart > tbody > tr > td.subtot > span").innerHTML
+  // console.log(typeof sub, typeof justText)
+  // sub.innerText = updateSubtot(product);
+  const allProducts = document.querySelectorAll('tr.product');
+  let totalPrice = 0;
+  allProducts.forEach(row => {
+    let price = row.children[1].children[0].innerText;
+    let qty = row.querySelector('.qty input').value;
+    let total = price*qty;
+    // totalPrice += total;
+    row.querySelector('.subtot span').innerText = total;
+  })
+  document.querySelectorAll('.subtot span').forEach(subtot => {
+    totalPrice += Number(subtot.innerText);
+  });
+
+ // let totalPrice = [...document.querySelectorAll('.subtot span')].reduce((acc, val) => acc+Number(val.innerText), 0);
+
+  document.querySelector("body > h2 > span").innerText = totalPrice;
+
 }
 $calc.onclick = calcAll;
 
 
+let row = `<tr class="product">
+<td class="name">
+  <span>Ironshirt</span>
+</td>
+
+<td class="pu">$<span>15.00</span></td>
+
+<td class="qty">
+  <label>
+    <input type="number" value="0" min="0" />
+  </label>
+</td>
+
+<td class="subtot">$<span>0</span></td>
+
+<td class="rm">
+  <button class="btn btn-delete">Delete</button>
+</td>
+</tr>`;
+// document.getele
+document.querySelector('#cart > tbody').innerHTML += row;
+
+// let td = document.createElement('td');
+// td.classList = 'qty'
+
+// td.appendChild(document.createElement('label'))
